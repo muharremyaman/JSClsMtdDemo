258afca390a8736945f8df1d7f5490f5d404ab94
Muharrem Yaman <muharrem2022@gmail.com>
2021-05-11 04:48:48 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.adi+"   isimli musteri Eklendi!");
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.adi+"   isimli musteri Silindi!");
+        }
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine(musteri.adi + " " + musteri.soyadi + " " + musteri.yasi + " " + musteri.Id);
+        }
+        
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.adi = "Mehmet";
+            musteri1.soyadi = "Yilmaz";
+            musteri1.Id = 1;
+            musteri1.yasi = 35;
+            
+            Musteri musteri2 = new Musteri();
+            musteri2.adi = "Ayşe";
+            musteri2.soyadi = "Dönmez";
+            musteri2.Id = 2;
+            musteri2.yasi = 27;
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.Ekle(musteri1);
+            musteriManager.Sil(musteri2);
+            musteriManager.Listele(musteri1);
+
+        }
+    }
+}
